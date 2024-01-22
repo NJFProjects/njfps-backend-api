5264112ef667612f88cb25b8eadcdc164153dfca
Huang Chuan <river1440>
2024-01-22 15:47:33 +0530

    chore: add pre-commit

--- app/route.py
+++ app/route.py
@@ -1,21 +1,21 @@
 from flask import Flask, request
 from flask_restx import Api, Resource
 
-aa
-
 app = Flask(__name__)
 api = Api(app)
 
 todos = {"todo1": "data=Remember the milk"}
 
-@api.route('/<string:todo_id>')
+
+@api.route("/<string:todo_id>")
 class TodoSimple(Resource):
     def get(self, todo_id):
         return {todo_id: todos[todo_id]}
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
+        todos[todo_id] = request.form["data"]
         return {todo_id: todos[todo_id]}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
