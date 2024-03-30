de61fb7bec5f9b67256d1911c8bc52b86f2f783b
Huang Chuan <river1440>
2024-03-30 18:31:03 +0530

    feat: add unit folder

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,12 @@
+import os
+
 from config import Config
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "test.db")
---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from app import create_app, db
+from tests import TestConfig
+
+
+# Pushing the app to context for the whole package
+@pytest.fixture(autouse=True, scope="package")
+def app_context_setup():
+    app = create_app(TestConfig)
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+    yield
+    db.session.remove()
+    db.drop_all()
+    app_context.pop()
