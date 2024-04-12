ba1be32ae18803479e0cc3075be30bfe92c75630
Huang Chuan <river1440>
2024-04-12 13:23:44 +0530

    chore: modify pytesy configuration, and uninstall flask_restx

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
-from flask_restx import Api
 
 bp = Blueprint("auth", __name__)
-api = Api(bp)
 
 from app.auth import route  # noqa: E402, F401
--- app/auth/route.py
+++ app/auth/route.py
@@ -1,37 +1,34 @@
 import sqlalchemy as sa
 from flask import request
-from flask_restx import Resource
 
 from app import db
-from app.auth import api
+from app.auth import bp
 from app.errors.errors import bad_request
 from app.models import Account
 
 
-@api.route("/accounts/<int:id>")
-class AccountApi(Resource):
-    def get(self, id):
-        # return "Hello World!"
-        return db.get_or_404(Account, id).to_dict()
+@bp.route("/accounts/<int:id>")
+def get_accounts(id):
+    # return "Hello World!"
+    return db.get_or_404(Account, id).to_dict()
 
 
-@api.route("/register")
-class RegisterApi(Resource):
-    def post(self):
-        data = request.get_json()
-        if "username" not in data or "email" not in data or "password" not in data:
-            return bad_request("must include username, email and password fields")
-        if db.session.scalar(
-            sa.select(Account).where(Account.username == data["username"])
-        ):
-            return bad_request("please use a different username")
-        if db.session.scalar(sa.select(Account).where(Account.email == data["email"])):
-            return bad_request("please use a different email address")
-        account = Account()
-        account.from_dict(data, new_user=True)
-        db.session.add(account)
-        db.session.commit()
-        return account.to_dict(), 201
+@bp.route("/register")
+def register():
+    data = request.get_json()
+    if "username" not in data or "email" not in data or "password" not in data:
+        return bad_request("must include username, email and password fields")
+    if db.session.scalar(
+        sa.select(Account).where(Account.username == data["username"])
+    ):
+        return bad_request("please use a different username")
+    if db.session.scalar(sa.select(Account).where(Account.email == data["email"])):
+        return bad_request("please use a different email address")
+    account = Account()
+    account.from_dict(data, new_user=True)
+    db.session.add(account)
+    db.session.commit()
+    return account.to_dict(), 201
 
 
 # @api.route('/users', methods=['GET'])
